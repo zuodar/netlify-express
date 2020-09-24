446139e36ef1e459c622ae8db4e92c7309fd828f
zuodar <45387732+zuodar@users.noreply.github.com>
2020-09-24 13:51:38 +0200

    add correct url

--- express/server.js
+++ express/server.js
@@ -1,16 +1,48 @@
 'use strict';
+require("dotenv").config();
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const fetch = require("node-fetch");
+
+const homePage = "https://tycho.pl/cstmpl";
+const url = `${homePage}/wp-json/wc/v3/products/batch?consumer_key=${process.env.consumer_key}&consumer_secret=${process.env.consumer_secret}`;
+const customProducts = {
+  create: [
+    {
+      name:
+        " \n <h3>S60 System do drzwi przesuwnych na 1 skrzydło drzwi <br /></h3> \n <h5>Prowadnica: 180 cm <br />\n Mocowanie: Do stropu <br />\n \n \n \n \n \n Miękkie domykanie: Nie</h5>",
+      regular_price: "115.60 zł",
+      short_description: 1,
+    },
+  ],
+};
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+
+ 
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(customProducts),
+    });
+    const json = await response.json();
+    const results = json;
+    return res.json({ success: true, results });
+  } catch (err) {
+    return res.status(500).json({ success: false, message: err.message });
+  }
 });
+
+
+
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
