c11d798714612c7118ca1e8c9578c9c43a5f407e
zuodar <45387732+zuodar@users.noreply.github.com>
2020-09-24 14:19:03 +0200

    update

--- express/server.js
+++ express/server.js
@@ -10,7 +10,7 @@ const fetch = require("node-fetch");
 const homePage = "https://tycho.pl/cstmpl";
 const url = `${homePage}/wp-json/wc/v3/products/batch?consumer_key=${process.env.consumer_key}&consumer_secret=${process.env.consumer_secret}`;
 const customProducts = {
-  create: [
+  create: [ 
     {
       name:
         " \n <h3>S60 System do drzwi przesuwnych na 1 skrzydło drzwi <br /></h3> \n <h5>Prowadnica: 180 cm <br />\n Mocowanie: Do stropu <br />\n \n \n \n \n \n Miękkie domykanie: Nie</h5>",
@@ -21,8 +21,6 @@ const customProducts = {
 };
 
 const router = express.Router();
-
- 
 
 app.get("/", async (req, res) => {
   try {
@@ -40,11 +38,6 @@ app.get("/", async (req, res) => {
     return res.status(500).json({ success: false, message: err.message });
   }
 });
-
-
-
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
