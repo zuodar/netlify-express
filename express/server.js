093d3ac414197549c3b83ea2b29c19ea2ecf1177
zuodar <45387732+zuodar@users.noreply.github.com>
2020-09-30 17:43:11 +0200

    test

--- express/server.js
+++ express/server.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 require("dotenv").config();
-require('encoding');
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
+require("encoding");
+const express = require("express");
+const path = require("path");
+const serverless = require("serverless-http");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
 var cors = require("cors");
 const homePage = "https://tycho.pl/cstmpl";
 const url = `${homePage}/wp-json/wc/v3/products/batch?consumer_key=${process.env.consumer_key}&consumer_secret=${process.env.consumer_secret}`;
 const customProducts = {
-  create: [ 
+  create: [
     {
       name:
         " \n <h3>S60 System do drzwi przesuwnych na 1 skrzydło drzwi <br /></h3> \n <h5>Prowadnica: 180 cm <br />\n Mocowanie: Do stropu <br />\n \n \n \n \n \n Miękkie domykanie: Nie</h5>",
@@ -25,31 +25,22 @@ const router = express.Router();
 
 app.use(cors());
 
-
 router.get("/", async (req, res) => {
   try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(customProducts),
-    });
-    const json = await response.json();
-    const results = json;
-    return res.json({ success: true, results });
+    const results = "test";
+    return res.json({ results });
   } catch (err) {
     return res.status(500).json({ success: false, message: err.message });
   }
 });
 
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
+router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
