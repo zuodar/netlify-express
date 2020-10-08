c2794cdede17043866126e14af76be2540a5940d
zuodar <45387732+zuodar@users.noreply.github.com>
2020-10-08 21:41:26 +0200

    cors

--- express/server.js
+++ express/server.js
@@ -1,72 +1,36 @@
 "use strict";
+require("dotenv").config();
+require("encoding");
+const cors = require("cors");
 const express = require("express");
+const path = require("path");
 const serverless = require("serverless-http");
 const app = express();
 const bodyParser = require("body-parser");
-
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Hello from Express.js! 20:34</h1>");
-  res.end();
-});
-router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
-router.post("/", (req, res) => res.json({ postBody: req.body, test: "20:34" }));
+const createProduct = require("./createProduct");
 
-app.use(bodyParser.json());
+app.use(cors());
+
+router.get("/", async (req, res) => {
+  const product = req.query.product;
+  if (!product) {
+    res.send("no product provided");
+    return;
+  }
+  createProduct(product)
+    .then((data) => {
+      res.send({
+        message: `Request received!, Created this product: ${product}`,
+      });
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/.netlify/functions/server", router); // path must route to lambda
-// app.use("/", router);
+app.use("/", router); // path must route to lambda
+
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
+
 module.exports = app;
 module.exports.handler = serverless(app);
-
-// "use strict";
-// require("dotenv").config();
-// require("encoding");
-// const cors = require("cors");
-
-// const express = require("express");
-// const path = require("path");
-// const serverless = require("serverless-http");
-// const app = express();
-// const bodyParser = require("body-parser");
-// const axios = require("axios");
-
-// const homePage = "https://tycho.pl/cstmpl";
-// const url = `${homePage}/wp-json/wc/v3/products/batch?consumer_key=${process.env.consumer_key}&consumer_secret=${process.env.consumer_secret}`;
-// const customProducts = {
-//   create: [
-//     {
-//       name:
-//         " \n <h3>S60 System do drzwi przesuwnych na 1 skrzydło drzwi <br /></h3> \n <h5>Prowadnica: 180 cm <br />\n Mocowanie: Do stropu <br />\n \n \n \n \n \n Miękkie domykanie: Nie</h5>",
-//       regular_price: "115.60 zł",
-//       short_description: 1,
-//     },
-//   ],
-// };
-
-// const router = express.Router();
-
-// app.use(cors());
-
-// function axiosTest() {
-//   const promise = axios.post(url, customProducts);
-//   const dataPromise = promise.then((response) => response.data);
-//   return dataPromise;
-// }
-
-// router.post("/", async (req, res) => {
-//   axiosTest()
-//     .then((data) => {
-//       console.log("I will be sending json now!");
-//       res.json({ message: "Request received!, 20:06", data });
-//     })
-//     .catch((err) => console.log(err));
-// });
-
-// app.use("/.netlify/functions/server", router); // path must route to lambda
-// // app.use("/", router); // path must route to lambda
-
-// app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
-
-// module.exports = app;
-// module.exports.handler = serverless(app);
