9dafad3b111d30fbcfcaf48896a66e9d83ca53f2
zuodar <45387732+zuodar@users.noreply.github.com>
2020-10-08 20:28:42 +0200

    cors

--- server-local.js
+++ server-local.js
@@ -1,5 +1,6 @@
 "use strict";
 
-const app = require("./express/server");
+// const app = require("./express/server");
+const app = require("./express/test");
 
 app.listen(3010, () => console.log("Local app listening on port 3010!"));
