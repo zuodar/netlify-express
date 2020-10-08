de0afb4caedf55e20385c10a9e96edda303088d5
zuodar <45387732+zuodar@users.noreply.github.com>
2020-10-08 21:49:15 +0200

    cors

--- express/server.js
+++ express/server.js
@@ -8,7 +8,29 @@ const serverless = require("serverless-http");
 const app = express();
 const bodyParser = require("body-parser");
 const router = express.Router();
-const createProduct = require("./createProduct");
+
+const homePage = "https://tycho.pl/cstmpl";
+const url = `${homePage}/wp-json/wc/v3/products/batch?consumer_key=${process.env.consumer_key}&consumer_secret=${process.env.consumer_secret}`;
+const customProducts = {
+  create: [
+    {
+      name:
+        " \n <h3>S60 System do drzwi przesuwnych na 1 skrzydło drzwi <br /></h3> \n <h5>Prowadnica: 180 cm <br />\n Mocowanie: Do stropu <br />\n \n \n \n \n \n Miękkie domykanie: Nie</h5>",
+      regular_price: "115.60 zł",
+      short_description: 1,
+    },
+  ],
+};
+
+function createProduct(product) {
+  console.log(
+    "im still using const from above, but I already got this data:",
+    JSON.parse(product)
+  );
+  const promise = axios.post(url, customProducts);
+  const dataPromise = promise.then((response) => response.data);
+  return dataPromise;
+}
 
 app.use(cors());
 
